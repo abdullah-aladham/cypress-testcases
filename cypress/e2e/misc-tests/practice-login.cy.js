03960ce4ec01ac867e9f9edaa18c530a5441b2e5
abdullah-aladham <abdullah.aladham@gmail.com>
2024-09-04 18:48:28 +0300

    added invalid testcases

--- cypress/e2e/misc-tests/practice-login.cy.js
+++ cypress/e2e/misc-tests/practice-login.cy.js
@@ -5,10 +5,23 @@ describe('Login-TestSuite',()=>{
         cy.get("input[id='password']").type("SuperSecretPassword!")
         cy.get("button[type='submit']").click();
     })
-    it('fails',()=>{
+    it('fails,Pass is incorrect',()=>{
         cy.visit("https://practice.expandtesting.com/login")
         cy.get("input[id='username']").type("practice")
+        cy.get("input[id='password']").type("SuperSecretPassword")
+        cy.get("button[type='submit']").click();
+    })
+    it("Fails , username is incorrect",()=>{
+        cy.visit("https://practice.expandtesting.com/login")
+        cy.get("input[id='username']").type("practic")
         cy.get("input[id='password']").type("SuperSecretPassword!")
         cy.get("button[type='submit']").click();
     })
+    it('Fails, invalid credentials',() => {
+        cy.visit("https://practice.expandtesting.com/login")
+        cy.get("input[id='username']").type("practic")
+        cy.get("input[id='password']").type("SuperSecretPassword")
+        cy.get("button[type='submit']").click();
+    })
+
 })
