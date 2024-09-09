b8efbce8eda693f32d330524b6d47f54d8f02e2d
abdullah-aladham <abdullah.aladham@gmail.com>
2024-09-09 19:59:43 +0300

    added cypress-real-events library

--- cypress/e2e/misc-tests/opensooq/mouseover.cy.js
+++ cypress/e2e/misc-tests/opensooq/mouseover.cy.js
@@ -1,8 +1,9 @@
 describe("mouseover Testsuite",()=>{
     it('pases',()=>{
-        cy.visit('https://ps.opensooq.com/ar')
+        cy.visit('https://ps.opensooq.com/ar');
         // cy.get('[data-id="realEstates"] > .relative').trigger('mouseover')
-        cy.get("a[href='/ar/عقارات']").invoke("show")
-        cy.get('body > div:nth-child(4) > header:nth-child(4) > nav:nth-child(1) > ul:nth-child(1) > li:nth-child(2) > ul:nth-child(2)').should('not.be.visible').invoke('show')
+        cy.get('body > div:nth-child(4) > header:nth-child(4) > nav:nth-child(1) > ul:nth-child(1) > li:nth-child(2)>a[href="/ar/عقارات"]').realHover();
+        //cy.get('li[data-id="realEstates"]').trigger('mouseover')
+        // cy.get('body > div:nth-child(4) > header:nth-child(4) > nav:nth-child(1) > ul:nth-child(1) > li:nth-child(2) > ul:nth-child(2)').should('not.be.visible').invoke('show')
     })
 })
